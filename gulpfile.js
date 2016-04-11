2830819b3ec82869e628fcc437a82baf74b656b8
Chad Christensen <chadthecoder@gmail.com>
2016-04-11 16:43:01 -0700

    Layout code icons, style project social link buttons

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,38 @@
-var gulp = require('gulp');
+var gulp   = require('gulp');
+var sass   = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var maps   = require('gulp-sourcemaps');
+var del    = require('del');
+
+gulp.task("minifyScripts", function() {
+  return gulp.src("./js/app.js")
+    .pipe(uglify())
+    .pipe(rename('app.min.js'))
+    .pipe(gulp.dest('./js'))
+});
+
+gulp.task('css', function() {
+  return gulp.src('./css/style.scss')
+  .pipe(maps.init())
+  .pipe(sass({
+    includePaths: ['./css/']
+  }))
+  .pipe(maps.write('./'))
+  .pipe(gulp.dest('./css'))
+});
+
+gulp.task('watchFiles', function() {
+  gulp.watch(['./css/*.scss'], ['css']);
+  // gulp.watch([''])
+});
+
+gulp.task('clean', function() {
+  del(['./css/style.css*']);
+});
+gulp.task("build", ['minifyScripts', 'css']);
+
+gulp.task('default', ['clean'], function() {
+  gulp.start('build');
+});
