6a73ac226d38afac99a0bdbbf8fec37e9b54ac1e
Chad Christensen <chadthecoder@gmail.com>
2016-05-14 07:55:01 -0700

    Fix smooth scrolling and make nav fixed

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
   var menuToggle = $('#js-mobile-menu').unbind();
-  var locationPath = filterPath(location.pathname);
-  var scrollElem = scrollableElement('html', 'body');
 
   $('#js-navigation-menu').removeClass("show");
 
@@ -13,50 +11,4 @@ $(document).ready(function() {
       }
     });
   });
-
-// Smooth Scrollin'
-  function filterPath(string) {
-    return string
-      .replace(/^\//,'')
-      .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
-      .replace(/\/$/,'');
-  }
-
-  $('a[href*="#"]').each(function() {
-    var thisPath = filterPath(this.pathname) || locationPath;
-    if (  locationPath == thisPath
-    && (location.hostname == this.hostname || !this.hostname)
-    && this.hash.replace(/#/,'') ) {
-      var $target = $(this.hash), target = this.hash;
-      if (target) {
-        var targetOffset = $target.offset().top;
-        $(this).click(function(event) {
-          event.preventDefault();
-          $(scrollElem).animate({scrollTop: targetOffset}, 400, function() {
-            location.hash = target;
-          });
-        });
-      }
-    }
-  });
-
-  // use the first element that is "scrollable"
-  function scrollableElement(els) {
-    for (var i = 0, argLength = arguments.length; i <argLength; i++) {
-      var el = arguments[i],
-          $scrollElement = $(el);
-      if ($scrollElement.scrollTop()> 0) {
-        return el;
-      } else {
-        $scrollElement.scrollTop(1);
-        var isScrollable = $scrollElement.scrollTop()> 0;
-        $scrollElement.scrollTop(0);
-        if (isScrollable) {
-          return el;
-        }
-      }
-    }
-    return [];
-  }
-
 });
